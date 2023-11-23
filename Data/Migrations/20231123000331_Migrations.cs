a53ad4faa5768335c8741202ade50762f102b048
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-11-22 19:08:11 -0500

    Agregar archivos de proyecto.

---
+++ Data/Migrations/20231123000331_Migrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppBlazor_LogIn.Data.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
